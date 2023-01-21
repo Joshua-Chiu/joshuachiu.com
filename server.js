d5b04967bf31a45debfcaf233079f870337a679e
Josh Chiu <joshchiu@student.ubc.ca>
2023-01-21 01:04:23 -0800

    installed bootstrap template 'personal' from bootstrapmade

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
+
+app.use(express.static('static'))
+
+/*
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+*/
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App started: listening on port ${port}`);
 })
